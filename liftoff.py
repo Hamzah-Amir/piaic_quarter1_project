f8a87f7f4774a53177dcd12fd4d0a8bbda137d3b
Hamzah-Amir <hamzaamir7651@gmail.com>
2024-11-05 00:29:45 +0500

    PIAIC Projects

---
+++ liftoff.py
@@ -0,0 +1,13 @@
+#Write a program that prints out the calls for a spaceship that is about to launch. Countdown 10 to 1 and then LIFTOFF!
+
+def liftoff():
+    
+    # using step size technique in for loop to reverse yhe countdown
+
+    for i in range(10, 0, -1):
+        print(i)
+    
+    print("LIFTOFF!")
+
+if __name__ == "__main__":
+    liftoff()
---
+++ random_num.py
@@ -0,0 +1,17 @@
+# Assignment no 4: Write a program to print 10 random numbers between 1 to 10
+
+from random import randint
+
+def random_num():
+    
+    i = 0
+
+    while i != 10:
+        
+        # Generating random number until value of i is less then 10
+        value = randint(1,100)
+        print(value)
+
+        i += 1 # Increasing value of i by 1 in each iteration
+
+random_num()
