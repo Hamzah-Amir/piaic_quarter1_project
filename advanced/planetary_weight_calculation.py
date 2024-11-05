63eb1ef153f4779b36a0ebc869010f34a996b1f9
Hamzah-Amir <hamzaamir7651@gmail.com>
2024-11-05 22:26:51 +0500

    PIAIC Q1 Project (Advance)

--- advanced/planetary_weight_calculation.py
+++ advanced/planetary_weight_calculation.py
@@ -11,22 +11,22 @@ def weight():
             print(f"According to NASA calculation your weight on Mars will be: {round(weight_earth*0.38, 2)}")
         
         elif other_planet_weight == "Mercury":
-            print(f"According to NASA calculation your weight on Mars will be: {round(weight_earth*0.378,2)}")
+            print(f"According to NASA calculation your weight on Mercury will be: {round(weight_earth*0.378,2)}")
         
         elif other_planet_weight == "Venus":
-            print(f"According to NASA calculation your weight on Mars will be: {round(weight_earth*0.889,2)}")
+            print(f"According to NASA calculation your weight on Venus will be: {round(weight_earth*0.889,2)}")
         
         elif other_planet_weight == "Jupiter":
-            print(f"According to NASA calculation your weight on Mars will be: {round(weight_earth*2.36,2)}")
+            print(f"According to NASA calculation your weight on Jupiter will be: {round(weight_earth*2.36,2)}")
         
         elif other_planet_weight == "Saturn":
-            print(f"According to NASA calculation your weight on Mars will be: {round(weight_earth*1.081,2)}")
+            print(f"According to NASA calculation your weight on Saturn will be: {round(weight_earth*1.081,2)}")
         
         elif other_planet_weight == "Uranus":
-            print(f"According to NASA calculation your weight on Mars will be: {round(weight_earth*0.815,2)}")
+            print(f"According to NASA calculation your weight on Uranus will be: {round(weight_earth*0.815,2)}")
         
         elif other_planet_weight == "Neptune":
-            print(f"According to NASA calculation your weight on Mars will be: {round(weight_earth*1.14,2)}")
+            print(f"According to NASA calculation your weight on Neptune will be: {round(weight_earth*1.14,2)}")
         
         else:
             print("You Entered invalid name of planet: ")
